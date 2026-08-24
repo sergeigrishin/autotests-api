cacaf73c0d50781207200f3f2a71ad876890dcb7
Sergei <sergeigrishin.go@gmail.com>
2026-08-24 14:59:36 +0300

    Практика написания  автотестов

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,9 +2,9 @@ from http import HTTPStatus
 
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
-# Импортируем функцию проверки статус-кода
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
+from tools.assertions.users import assert_create_user_response
 
 
 def test_create_user():
@@ -14,12 +14,13 @@ def test_create_user():
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
-    # Используем функцию для проверки статус-кода
     assert_status_code(response.status_code, HTTPStatus.OK)
 
     assert response_data.user.email == request.email, 'Некорректный email пользователя'
     assert response_data.user.last_name == request.last_name, 'Некорректный last_name пользователя'
     assert response_data.user.first_name == request.first_name, 'Некорректный first_name пользователя'
     assert response_data.user.middle_name == request.middle_name, 'Некорректный middle_name пользователя'
+
+    assert_create_user_response(request, response_data)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
