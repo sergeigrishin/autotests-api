a90c344a5f2703e47eb95a7cb9d816ef65c12dbf
Sergei <sergeigrishin.go@gmail.com>
2026-08-25 10:45:28 +0300

    Плагины ytest

---
+++ tests/test_usefixtures.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+# Фикстура для очистки данных из базы данных
+@pytest.fixture
+def clear_books_database():
+    print("[FIXTURE] Удаляем все данные из базы данных")
+
+
+# Фикстура для заполнения данных в базу данных
+@pytest.fixture
+def fill_books_database():
+    print("[FIXTURE] Создаем новые данные в базе данных")
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,26 +1,36 @@
 from http import HTTPStatus
 
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+import pytest
+
+from clients.users.private_users_client import PrivateUsersClient
+from clients.users.public_users_client import PublicUsersClient
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
 
 
-def test_create_user():
-    public_users_client = get_public_users_client()
-
+@pytest.mark.users
+@pytest.mark.regression
+def test_create_user(public_users_client: PublicUsersClient):
     request = CreateUserRequestSchema()
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
-
-    assert response_data.user.email == request.email, 'Некорректный email пользователя'
-    assert response_data.user.last_name == request.last_name, 'Некорректный last_name пользователя'
-    assert response_data.user.first_name == request.first_name, 'Некорректный first_name пользователя'
-    assert response_data.user.middle_name == request.middle_name, 'Некорректный middle_name пользователя'
-
     assert_create_user_response(request, response_data)
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(function_user: UserFixture, private_users_client: PrivateUsersClient):
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_get_user_response(response_data, function_user.response)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
